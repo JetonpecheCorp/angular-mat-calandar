118bd2bf15fba53fd838a91993b779506665084f
jetonpeche <57717713+jetonpeche@users.noreply.github.com>
2026-06-04 17:16:16 +0200

    Add files via upload

---
+++ src/models/SidebarConfigCalandar.ts
@@ -0,0 +1,8 @@
+export type SidebarConfigCalandar =
+{
+    title: string;
+    defaultOpen: boolean,
+    accordeonMod: 'expanded' | 'expendedFirst';
+    hideToggle: boolean,
+    multi: boolean
+}
